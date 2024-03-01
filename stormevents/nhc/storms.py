116ecbfbfdfb8d0733397d95e237c9eef6866bae
ahijevyc <ahijevyc@ucar.edu>
2024-03-01 09:19:27 -0700

    Avoid depreciation warning in pd.read_csv
    Instead of date_parser, use date_format
    Intead of x.strip() use skipinitialspace=True
    Instead of if-clause, use na_values=["9"*10]

--- stormevents/nhc/storms.py
+++ stormevents/nhc/storms.py
@@ -71,11 +71,9 @@ def nhc_storms(year: int = None) -> pandas.DataFrame:
             header=0,
             names=columns,
             parse_dates=["start_date", "end_date"],
-            date_parser=lambda x: (
-                pandas.to_datetime(x.strip(), format="%Y%m%d%H")
-                if x.strip() != "9999999999"
-                else numpy.nan
-            ),
+            date_format="%Y%m%d%H",
+            na_values = ["9"*10],
+            skipinitialspace=True,
         )
 
     storms = storms.astype(
